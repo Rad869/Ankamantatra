6dbc1d0a3ffc30f4a366f3c9ab62ce9c2fe2cb82
HeryRA <annaelle.hery.co@gmail.com>
2024-06-24 21:29:58 +0800

    protect fgets against possible overflow attacks

--- sources/play.c
+++ sources/play.c
@@ -14,12 +14,18 @@
 
 static void	check_answer(t_game *game, char *answer)
 {
+	int	i;
+
+	i = 0;
 	lower_case(answer);
 	lower_case(game->answer);
+	while (game->answer[i] != '\n')
+		i++;
+	game->answer[i] = '\0';
 	if (strcmp(answer, game->answer) == 0)
 		printf(GRN"\nMarina\n\n"CRESET);
 	else
-		printf(RED"\nDiso\n\n"CRESET);
+		printf(RED"\nDiso - %s anefa izany!\n\n"CRESET, game->answer);
 	sleep(1);
 }
 
@@ -30,6 +36,32 @@ static void	init_question(t_game *game)
 	rand_num = get_random_num();
 	game->question = get_line("data/questions.txt", rand_num);
 	game->answer = get_line("data/responses.txt", rand_num);
+}
+
+void	empty_buf()
+{
+	int	c;
+
+	c = 0;
+	while (c != '\n' && c != EOF)
+		c = getc(stdin);
+}
+
+void	read_answer(char *str, int len)
+{
+	char	*pos_line_break;
+
+	pos_line_break = NULL;
+	if (fgets(str, len, stdin) != NULL)
+	{
+		pos_line_break = strchr(str, '\n');
+		if (pos_line_break != NULL)
+			*pos_line_break = '\0';
+		else
+			empty_buf();
+	}
+	else
+		empty_buf();
 }
 
 void	play(t_game *game)
@@ -43,8 +75,9 @@ void	play(t_game *game)
 		printf("================\n Fanontaniana %d\n================\n", i);
 		init_question(game);
 		printf("%s", game->question);
-		fgets(answer, 100, stdin);
+		read_answer(answer, 100);
 		check_answer(game, answer);
 		free_resources(*game);
+		fget
 	}
 }
